0b754224c774315e949f1e2400bf9a776da52969
Salvatore Lombardo <salvolombardoj@gmail.com>
2025-12-05 12:55:13 +0100

    Fix Allowed_hosts

--- atelier_project/atelier_project/settings.py
+++ atelier_project/atelier_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mgnq02y6q1usvrpjkv(aaw6ob$h0_^=v0eg09nn06487tba5hl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.61.209.228']
 
 
 
